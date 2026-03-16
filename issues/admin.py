cd75e0920e35979bf8296b68e73aeeb9ef83898b
Mariia Bidenko <mbidenk@gmail.com>
2026-03-16 17:14:22 +0100

    Edit all models to the administration interface

--- issues/admin.py
+++ issues/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+#user password: mashaSUPER11
+
+from models import IssueType, Resolution, Issue, Comment
+
+admin.site.register(IssueType)
+admin.site.register(Resolution)
+admin.site.register(Issue)
+admin.site.register(Comment)
